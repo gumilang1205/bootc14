7271c3673fccc3239861ce43df40a8042acb78cf
Gumilang <gmandiri86@gmail.com>
2025-07-14 14:38:47 +0700

    tugas1

---
+++ TugasPertama/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+class program
+{
+    static void Main(String[] args)
+    {
+        int[] a = new int[15];
+        for (int i = 1; i <=a.Length; i++)
+        {
+            if (i % 3 == 0 && i % 5 ==0)
+                Console.Write("foobar");
+            else if (i % 5 == 0)
+                Console.Write("bar,");
+            else if (i % 3 == 0)
+                Console.Write("foo,");
+            else
+                Console.Write(i+",");
+        }
+
+    }
+}
