82c6732b83f25634adb4b6350af484e4a5fd2080
Gumilang <gmandiri86@gmail.com>
2025-07-16 11:19:17 +0700

    1

---
+++ LearningClasses/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+namespace Classes
+{
+    public class Car
+    {
+        public string Merk;
+        public string Ukuran;
+        public int Kecepatan;
+        public Car(string merk, string ukuran, int kecepatan)
+        {
+             Merk = merk;
+             Ukuran = ukuran;
+             Kecepatan = kecepatan;
+        }
+        void CityCar()
+        {
+            Console.WriteLine($"ukuran mobil city car {Ukuran} dan kecepatan maksimal {Kecepatan} km/jam");
+        }
+        void Informasi()
+        {
+            Console.WriteLine($"mobil ini adalah buatan {Merk} ukuran mobil {Ukuran} dan kecepatannya {Kecepatan} km/jam");
+        }
+
+        
+     static void Main(string[] args)
+        {
+            Car sedan = new Car("toyota", "sedang", 80);
+            sedan.CityCar();
+            sedan.Informasi();
+
+        }
+    }
+}
