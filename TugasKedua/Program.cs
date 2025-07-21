76ce23339e58746534a2ed939c6ee08704cea3dc
gumilang1205 <gumilangcm4@gmail.com>
2025-07-21 18:00:28 +0700

    tugas2

---
+++ TugasKedua/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static int angka;
+
+    static void Main(String[] args)
+    {
+        Console.Write("Masukkan Maksimal Angka : ");
+        angka = int.Parse(Console.ReadLine());
+        Condition();
+    }
+    static void Condition()
+    {
+        for (int i = 1; i <= angka; i++)
+        {
+            if (i % 7 == 0 && i % 5 == 0 && i % 3 == 0)
+                Console.Write("foobarjazz,");
+            else if (i % 7 == 0 && i % 5 == 0)
+                Console.Write("barjazz,");
+            else if (i % 7 == 0 && i % 3 == 0)
+                Console.Write("foojazz,");
+            else if (i % 7 == 0)
+                Console.Write("jazz,");
+            else if (i % 3 == 0 && i % 5 == 0)
+                Console.Write("foobar,");
+            else if (i % 5 == 0)
+                Console.Write("bar,");
+            else if (i % 3 == 0)
+                Console.Write("foo,");
+            else
+                Console.Write(i + ",");
+        }
+    }
+}
--- TugasPertama/Program.cs
+++ TugasPertama/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-class program
+﻿class program
 {
     static void Main(String[] args)
     {
